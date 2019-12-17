464b0f8a0ebae8a7404ff06b56647c2d193c31ea
ETI <ETI@192.168.1.10>
2019-12-17 06:48:55 -0500

    First Commit

---
+++ src/Test1.java
@@ -0,0 +1,15 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		String hungry = "Yes";
+		if (hungry == "Yes")
+			System.out.println(" Eat Smthng");
+		else {
+			System.out.println("Do some work");
+		}
+		
+
+	}
+
+}
